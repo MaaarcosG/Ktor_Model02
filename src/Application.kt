525d0740f137f4350f58599208a513d1f5db7412
Marcos <gut17909@uvg.edu.gt>
2020-12-15 16:15:48 -0600

    update

--- src/Application.kt
+++ src/Application.kt
@@ -1,17 +1,18 @@
 package com.example_ktorYT
 
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.gson.*
 import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
+@JvmOverloads
 fun Application.module(testing: Boolean = false) {
     install(ContentNegotiation) {
         gson { }
@@ -48,10 +49,6 @@ fun Application.task(){
                         false -> call.respond(HttpStatusCode.NotFound, Error("Task with id $id not found"))
                     }
             }
-            /*delete*/
-            delete {
-                task_list.remove(call.receive())
-            }
             get("/{id}"){
                 val data: Long? = call.parameters["id"]?.toLongOrNull()
                 val result: Unit? = when(data){
@@ -76,10 +73,10 @@ fun Application.task(){
                     else -> {
                         val task_lt = task_list.firstOrNull { it.id == data }
                         when(task_lt){
-                            null -> call.respond(HttpStatusCode.NotFound, Error("User ID $data not found"))
+                            null -> call.respond(HttpStatusCode.NotFound, Error("Task ID $data not found"))
                             else -> {
                                 task_list.remove(task_lt)
-                                call.respond(HttpStatusCode.OK, Error("User ID $data deleted"))
+                                call.respond(HttpStatusCode.OK, Error("Task ID $data deleted"))
                             }
                         }
                     }
@@ -88,7 +85,7 @@ fun Application.task(){
 
             delete("/remove"){
                 task_list.clear()
-                call.respond(HttpStatusCode.OK, Error("All data is remove"))
+                call.respond(HttpStatusCode.OK, Error("All data is remove $task_list"))
             }
         }
     }
