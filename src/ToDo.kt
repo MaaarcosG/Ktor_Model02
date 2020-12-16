3a29d63029720e7ed2aa463edadcfad61eb09fea
Marcos <gut17909@uvg.edu.gt>
2020-12-15 20:25:15 -0600

    remove override

--- src/ToDo.kt
+++ src/ToDo.kt
@@ -1,12 +1,4 @@
 package com.example_ktorYT
 
-data class Item(val id: Long?, val task: String?, val complete: Boolean?){
-    override fun equals(other: Any?): Boolean{
-        if(other is Item){
-            return other.id == id
-        }
-        return super.equals(other)
-    }
-    override fun hashCode(): Int = id. hashCode()
-}
+data class Item(val id: Long?, val task: String?, val complete: Boolean?)
 data class Error(val message: String?)
